1fd72c477e553763611f73ebfb444a432749d1b5
Bhavya Anil <bhavya.anil2023@gmail.com>
2026-03-10 18:21:48 -0700

    added game over/restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,14 @@ public class GameManager : MonoBehaviour
     public PlayerController PlayerController;
     public TurnManager TurnManager { get; private set; }
 
-    private int m_FoodAmount = 100;
+    private int m_FoodAmount = 10;
     public UIDocument UIDoc;
     private Label m_FoodLabel;
 
     private int m_CurrentLevel = 1;
+
+    private VisualElement m_GameOverPanel;
+    private Label m_GameOverMessage;
 
     private void Awake()
     {
@@ -26,13 +29,31 @@ public class GameManager : MonoBehaviour
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    { 
+    {
         TurnManager = new TurnManager();
         TurnManager.OnTick += OnTurnHappen;
-        NewLevel();
 
         m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel");
-        m_FoodLabel.text = "Food: " + m_FoodAmount;
+
+        m_GameOverPanel = UIDoc.rootVisualElement.Q<VisualElement>("GameOverPanel");
+        m_GameOverMessage = m_GameOverPanel.Q<Label>("GameOverMessage");
+
+        StartNewGame();
+    }
+
+    public void StartNewGame()
+    {
+        m_GameOverPanel.style.visibility = Visibility.Hidden;
+
+        m_CurrentLevel = 1;
+        m_FoodAmount = 20;
+        m_FoodLabel.text = "Food : " + m_FoodAmount;
+
+        BoardManager.Clean();
+        BoardManager.Init();
+
+        PlayerController.Init();
+        PlayerController.Spawn(BoardManager, new Vector2Int(1, 1));
     }
 
     public void NewLevel()
@@ -52,6 +73,14 @@ public class GameManager : MonoBehaviour
     {
         m_FoodAmount += amount;
         m_FoodLabel.text = "Food: " + m_FoodAmount;
+
+        if (m_FoodAmount <= 0)
+        {
+            PlayerController.GameOver();
+            m_GameOverPanel.style.visibility = Visibility.Visible;
+            m_GameOverMessage.text = "Game Over!\n\nYou completed " + m_CurrentLevel + " levels\n\nPress enter to restart";
+
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
 {
     private BoardManager m_Board;
     private Vector2Int m_CellPosition;
+    private bool m_IsGameOver;
 
     public void Spawn(BoardManager boardManager, Vector2Int cell)
     {
@@ -18,6 +19,16 @@ public class PlayerController : MonoBehaviour
         transform.position = m_Board.CellToWorld(m_CellPosition);
     }
 
+    public void GameOver()
+    {
+        m_IsGameOver = true;
+    }
+
+    public void Init()
+    {
+        m_IsGameOver = false;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,6 +38,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (m_IsGameOver)
+        {
+            if (Keyboard.current.enterKey.wasPressedThisFrame)
+            {
+                GameManager.Instance.StartNewGame();
+            }
+
+            return;
+        }
         Vector2Int newCellTarget = m_CellPosition;
         bool hasMoved = false;
 
